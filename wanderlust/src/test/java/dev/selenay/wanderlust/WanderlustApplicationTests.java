a85d13fff0f9b81eb41ea3d5e30bcc09f1b2807e
Selenay Buse Batıbay <129968145+SelenayBuse@users.noreply.github.com>
2023-12-08 18:54:57 +0300

    init

---
+++ wanderlust/src/test/java/dev/selenay/wanderlust/WanderlustApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.selenay.wanderlust;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WanderlustApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
